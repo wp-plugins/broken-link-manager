8869a547b4795449d935ed30b4dd0c38ad6c27f6
flashcentury <flashcentury@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-21 08:21:17 +0000

    Subver. 0.3.5
    git-svn-id: https://plugins.svn.wordpress.org/broken-link-manager/tags/0.3.0@1095758 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wblm-redirect-url.php
@@ -0,0 +1,52 @@
+<?php
+global $WblmListTable;
+$s  = isset($_REQUEST['s']) ? $_REQUEST['s'] : null;
+$WblmListTable->prepare_items($s);
+?>
+<div id="wrapper" class="wblm_tables">
+	<!-- Navigation -->
+	<nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+		<div class="navbar-header">
+			<a class="navbar-brand" href="<?php echo admin_url('admin.php?page=wblm-dashboard'); ?>">
+			<?php echo WBLM_NAME; ?> <span> Ver <?php echo WBLM_VERSION; ?></span>
+			</a>
+		</div>
+	</nav>
+	<div id="page-wrapper">
+		<div class="row">
+			<div class="col-lg-12">
+				<h3 class="page-header"><?php _e('Redirected URLs', 'wblm') ?></h3>
+				<?php get_wblmTopNavi(); ?>
+			</div>
+			<!-- /.col-lg-12 -->
+		</div>            
+		<!-- /.row -->
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<?php _e('Redirected URLs', 'wblm') ?>
+						<form method="get">
+					    <input type="hidden" name="page" value="<?php echo $_REQUEST['page'] ?>" />
+					    <?php $WblmListTable->search_box( 'search', 'search_id' ); ?>
+						</form>
+					</div>
+					<!-- /.panel-heading -->
+					<div class="panel-body">
+						<div class="table-responsive">
+						<form action="<?php echo admin_url('admin.php?page=wblm-redirect'); ?>" id="wpse-list-table-form" method="post">
+						<?php $WblmListTable->display(); ?> 
+						</form>
+						</div>
+						<!-- /.table-responsive -->
+					</div>
+					<!-- /.panel-body -->
+				</div>
+				<!-- /.panel -->
+			</div>
+			<!-- /.col-lg-12 -->
+		</div>            
+	</div>
+	<!-- /#page-wrapper -->
+	<?php get_wblmFooter(); ?>
+</div>
