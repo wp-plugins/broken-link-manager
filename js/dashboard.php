8598b6cb3736bd77fadac37d77afc60b576ea07f
flashcentury <flashcentury@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-21 08:21:17 +0000

    Subver. 0.3.5
    git-svn-id: https://plugins.svn.wordpress.org/broken-link-manager/tags/0.2.7@1095758 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/dashboard.php
@@ -0,0 +1,71 @@
+<?php
+   //Let's set the header straight
+   header('Content-type: text/javascript');
+
+   //Get the WP-specifics, so that we can use constants and what not
+   $home_dir = preg_replace('^wp-content/plugins/[a-z0-9\-/]+^', '', getcwd());
+   include($home_dir . 'wp-load.php');
+   global $wpdb;
+   
+echo"
+(function($) { 
+$(function() {
+	Morris.Line({ 
+	element: 'morris-area-chart',
+    	data: ["; 
+        
+
+$logstats = $wpdb->get_results("SELECT DATE_FORMAT(`date` , '%Y-%m-%d') as date, COUNT(id) as `impressions`, SUM(redirect) as `redirect`, SUM(broken) as `broken` FROM `".TABLE_WBLM_LOG."` GROUP BY DATE_FORMAT(`date` , '%Y-%m-%d') DESC LIMIT 15");
+
+if ($logstats) {
+   foreach ($logstats as $log) {   
+   echo "{
+            period: '$log->date',
+            total: $log->impressions,
+            redirect: $log->redirect,
+            broken: $log->broken
+        },"; 
+   
+   }
+}
+
+
+echo "], \n
+        xkey: 'period',
+        ykeys: ['total', 'redirect', 'broken'],
+        labels: ['Total', '301', '404'],
+        pointSize: 2,
+        hideHover: 'auto',
+        resize: true,
+        lineColors: ['#428bca','#5cb85c','#d9534f']
+    }); \n \n";
+
+
+
+
+$redirectUrlsCount = $wpdb->get_var("SELECT COUNT(id) FROM " . TABLE_WBLM . " where `active` = '1'" );
+$brokenlinkCount = $wpdb->get_var("SELECT COUNT(id) FROM " . TABLE_WBLM . " where `active` = '0'" );
+
+
+echo "Morris.Donut({
+        element: 'morris-donut-chart',
+        data: [{
+            label: 'Redirect (301)',
+            value: $redirectUrlsCount
+        }, {
+            label: 'Broken (404)',
+            value: $brokenlinkCount
+        }],
+        resize: true
+    });
+
+});
+
+})(jQuery);";
+   
+   
+   
+   
+   
+   
+?>
